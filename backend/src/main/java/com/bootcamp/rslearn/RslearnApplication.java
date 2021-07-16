59047481a4db8ba727047c328e0203be5d693b28
rodrigoschlichta <rodrigo.schlichta@gmail.com>
2021-07-16 11:52:11 -0300

    Backend Project Created

---
+++ backend/src/main/java/com/bootcamp/rslearn/RslearnApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RslearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bootcamp/rslearn/RslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
