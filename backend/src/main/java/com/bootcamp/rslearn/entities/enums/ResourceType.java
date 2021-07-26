9a24a3c689163e3cb7a4251ff3c861215cef8ef2
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-07-26 15:18:22 -0300

    Resource

---
+++ backend/src/main/java/com/bootcamp/rslearn/entities/enums/ResourceType.java
@@ -0,0 +1,8 @@
+package com.bootcamp.rslearn.entities.enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLY ,LESSON_TASK, FORUN, EXTERNAL_LINK;
+	
+	
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -17,3 +17,5 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp', 'https:/
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);
+
+INSERT INTO (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha Html','Trilha principal do Curso', 1, 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 1, 1);
