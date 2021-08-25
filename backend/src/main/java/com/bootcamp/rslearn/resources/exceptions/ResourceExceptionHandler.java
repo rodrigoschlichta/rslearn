e87719f35e27ba50cf1386400e32a1edc94c9a3a
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-08-25 10:38:26 -0300

    Web Security

---
+++ backend/src/main/java/com/bootcamp/rslearn/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,102 @@
+package com.bootcamp.rslearn.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.bootcamp.rslearn.services.exceptions.DataBaseException;
+import com.bootcamp.rslearn.services.exceptions.ResourceNotFoundException;
+
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	HttpStatus status = HttpStatus.NOT_FOUND;	
+	StandardError err = new StandardError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("Resource not Found");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+	StandardError err = new StandardError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("Data Base Exception");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	
+    }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+    HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+	ValidationError err = new ValidationError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("Validation Exception");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	
+	for (FieldError f : e.getBindingResult().getFieldErrors()) {
+		err.addError(f.getField(), f.getDefaultMessage());
+	}
+	
+	return ResponseEntity.status(status).body(err);
+	
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request){
+	HttpStatus status = HttpStatus.BAD_REQUEST;	
+	StandardError err = new StandardError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("AWS Exception");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request){
+	HttpStatus status = HttpStatus.BAD_REQUEST;	
+	StandardError err = new StandardError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("AWS Exception");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<StandardError> illegalArgument(IllegalArgumentException e, HttpServletRequest request){
+	HttpStatus status = HttpStatus.BAD_REQUEST;	
+	StandardError err = new StandardError();
+	err.setTimestamp(Instant.now());
+	err.setStatus(status.value());
+	err.setError("Bad Request");
+	err.setMessage(e.getMessage());
+	err.setPath(request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+}
