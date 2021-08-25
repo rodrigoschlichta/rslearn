991a3619f4f7a6a15f12753f8e22426c8fa605df
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-08-25 15:48:24 -0300

    Validation

--- backend/src/main/java/com/bootcamp/rslearn/services/UserService.java
+++ backend/src/main/java/com/bootcamp/rslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.bootcamp.rslearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bootcamp.rslearn.dto.UserDTO;
 import com.bootcamp.rslearn.entities.User;
 import com.bootcamp.rslearn.repositories.UserRepository;
+import com.bootcamp.rslearn.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService{
@@ -18,6 +23,14 @@ public class UserService implements UserDetailsService{
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional <User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity Not Found"));
+		return new UserDTO(entity);
+		
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
